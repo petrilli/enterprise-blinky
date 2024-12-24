d5582dfea752aedb6cbc4f06353345fbad52bd4c
Christopher Petrilli <petrilli@amber.org>
2024-12-24 13:53:15 -0800

    feat:  add NVS storage and randomness
    Added an NVS storage variable that keeps track of the number of reboots the device has gone through since the last flash. Moved the blink time to a random number between 100-1100.

--- src/main.c
+++ src/main.c
@@ -1,30 +1,84 @@
 /**
  * @file main.c
  * @brief A somewhat complicated example of blinking an LED in Zephyr with some logging over a faux USB serial port.
+ *
+ * A few of the things that are demonstrsated are GPIO, logging, USB, random numbers, and finally NVS storage for a
+ * reboot counter.
  */
-
 #include <stdio.h>
 #include <zephyr/bindesc.h>
+#include <zephyr/device.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/fs/nvs.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/random/random.h>
+#include <zephyr/storage/flash_map.h>
 #include <zephyr/usb/usb_device.h>
 
 
 LOG_MODULE_REGISTER(main);
 
-/// Time to sleep between blinks, where 1000 msec = 1 sec
-#define SLEEP_TIME_MS   250
+/// Minimum time to sleep between state changes in milliseconds
+#define BASE_SLEEP_TIME_MS    100
+/// Maximum time to sleep between blinks is BASE_SLEEP_TIME_MS + MAX_SLEEP_TIME_MS
+#define MAX_SLEEP_TIME_MS   1001
 
 /// Devicetree identifier alias for the LED to blink
 #define LED0_NODE DT_ALIAS(led0)
 
+#define NVS_PARTITION storage_partition
+#define NVS_PARTITION_DEVICE FIXED_PARTITION_DEVICE(NVS_PARTITION)
+#define NVS_PARTITION_OFFSET FIXED_PARTITION_OFFSET(NVS_PARTITION)
+#define NVS_DATA_REBOOT_COUNT_ID 1
+
+int nvs_initialize(struct nvs_fs *fs) {
+    struct flash_pages_info page_info; //< Page info for NVS
+    int ret;                           //< Default return collector
+
+    /* Define the NFS filesystem */
+    fs->flash_device = NVS_PARTITION_DEVICE;
+    if (!device_is_ready(fs->flash_device)) {
+        LOG_ERR("Flash device not ready");
+        return -1;
+    } else {
+        LOG_INF("Flash device ready");
+    }
+    fs->offset = NVS_PARTITION_OFFSET;
+    ret = flash_get_page_info_by_offs(fs->flash_device, fs->offset, &page_info);
+    if (ret > 0) {
+        LOG_ERR("Failed to get page info for NVS (return = %d)", ret);
+        return -1;
+    }
+    fs->sector_size = page_info.size;
+    fs->sector_count = 2U;
+
+    ret = nvs_mount(fs);
+    if (ret > 0) {
+        LOG_ERR("Failed to mount NVS (return = %d)", ret);
+        return -1;
+    }
+    return 0;
+}
+
+/**
+ * Generate a random delay for use later
+ *
+ * @return random delay between 0 and 999
+ */
+uint16_t random_delay_get(void) {
+    uint32_t rand_val = sys_rand32_get();
+    return (uint16_t) (rand_val % MAX_SLEEP_TIME_MS) + BASE_SLEEP_TIME_MS;
+}
 
 int main(void) {
     int ret;                                                                   //< General return value collector
     const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console)); //< UART for the console device
-    uint32_t dtr = 0;                                                          //< DTR status
+    uint32_t dtr = 0U;                                                         //< DTR status
+    uint32_t reboot_count = 0U;                                                //< Reboot counter
+    static struct nvs_fs fs;                                                   //< NVS filesystem
     static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios); //< Build error w/o LED defined
 
     /*
@@ -41,6 +95,26 @@ int main(void) {
     }
 
     LOG_INF("Greetings developer! (%s @ %s)", CONFIG_BOARD, BINDESC_GET_STR(build_date_time_string));
+
+    ret = nvs_initialize(&fs);
+    if (ret < 0) {
+        LOG_ERR("Failed to initialize NVS");
+        return 0;
+    }
+
+    /* Get the reboot count */
+    ret = nvs_read(&fs, NVS_DATA_REBOOT_COUNT_ID, &reboot_count, sizeof(reboot_count));
+    if (ret > 0) {
+        LOG_INF("Reboot count: %d at id=%d", reboot_count, NVS_DATA_REBOOT_COUNT_ID);
+        reboot_count++;
+        LOG_INF("Setting reboot count to %d at id=%d", reboot_count, NVS_DATA_REBOOT_COUNT_ID);
+        (void) nvs_write(&fs, NVS_DATA_REBOOT_COUNT_ID, &reboot_count, sizeof(reboot_count));
+        // XXX Do something with return
+    } else {
+        LOG_INF("No reboot count found at id=%d. Initializing to 0.", NVS_DATA_REBOOT_COUNT_ID);
+        (void) nvs_write(&fs, NVS_DATA_REBOOT_COUNT_ID, &reboot_count, sizeof(reboot_count));
+        // XXX Do something with return
+    }
 
     if (!gpio_is_ready_dt(&led)) {
         LOG_ERR("LED device not ready");
@@ -61,9 +135,9 @@ int main(void) {
         if (ret != 0) {
             return 0;
         }
-
-        LOG_INF("LED state: %s", gpio_pin_get_dt(&led) ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
+        uint16_t delay = random_delay_get();
+        LOG_INF("LED state: %s (%dms)", gpio_pin_get_dt(&led) ? "ON" : "OFF", delay);
+        k_msleep(delay);
     }
     return 0;
 }
